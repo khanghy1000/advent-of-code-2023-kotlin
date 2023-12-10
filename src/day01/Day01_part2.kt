f8d6122460474433b9bc866ead6d3a25d1e6d9e1
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2023-12-10 22:48:20 +0700

    Day 1: Add part 2 answer

---
+++ src/day01/Day01_part2.kt
@@ -0,0 +1,54 @@
+package day01
+
+import readInput
+
+val SPELLED_OUT_NUMBERS = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9
+)
+
+fun String.getCalibrationValueWithSpelledOutNum(): Int {
+    val spelledOutNums = SPELLED_OUT_NUMBERS.map {
+        mapOf(
+            "num" to it.value,
+            "index" to this.indexOf(it.key)
+        )
+    } + SPELLED_OUT_NUMBERS.map {
+        mapOf(
+            "num" to it.value,
+            "index" to this.lastIndexOf(it.key)
+        )
+    }
+
+    val firstDigit =
+        mapOf(
+            "num" to (this.getFirstDigit()?.digitToInt() ?: 0),
+            "index" to this.indexOf(this.getFirstDigit().toString())
+        )
+    val lastDigit =
+        mapOf(
+            "num" to (this.getLastDigit()?.digitToInt() ?: 0),
+            "index" to this.lastIndexOf(this.getLastDigit().toString())
+        )
+
+    val nums = (spelledOutNums + firstDigit + lastDigit).filter { it["index"] != -1 }
+
+    val numIndexes = nums.map { it["index"] ?: -1 }
+
+    val firstNum = nums.filter { it["index"] == numIndexes.min() }[0]["num"]
+    val lastNum = nums.filter { it["index"] == numIndexes.max() }[0]["num"]
+
+    return "$firstNum$lastNum".toInt()
+}
+
+fun main() {
+    val input = readInput("day01/Day01")
+    println(input.sumOf { it.getCalibrationValueWithSpelledOutNum() })
+}
